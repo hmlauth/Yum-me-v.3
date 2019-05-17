12b79c3f8354b2c799cfbecf84838f521c2d7593
Andromeda <hannalauth@gmail.com>
2019-05-16 22:27:57 -0700

    recipe in two columns on page

--- client/src/pages/Develop/develop.js
+++ client/src/pages/Develop/develop.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
+import Container from "../../components/Container";
+import { Row, Col } from "../../components/Grid";
+import Header from "../../components/Header"
 
 class Develop extends Component {
 
@@ -7,7 +10,12 @@ class Develop extends Component {
         super(props);
         this.state = {
             recipes: [],
-            recipe: []
+            recipe: [],
+            Ingredients: [],
+            topIngredients: [],
+            Instructions: [],
+            topInstructions: [],
+            comments: []
         }
     }
 
@@ -16,14 +24,70 @@ class Develop extends Component {
         console.log(id)
         API.getVersions(id)
         .then(res => {
-            console.log("getVersions", res)
+            console.log("getVersions", res.data)
+            this.setState({
+                recipes: res.data,
+                recipe: res.data[0],
+                Ingredients: res.data[0].extendedIngredients.Ingredients,
+                topIngredients: res.data[0].extendedIngredients.Toppings,
+                Instructions: res.data[0].extendedInstructions.Instructions,
+                topInstructions: res.data[0].extendedInstructions.Topping
+            })
         })
+
+        // API for when comments are ready to be populated
+        // API.getComments(id)
+        // .then(res => {
+        //     console.log("getComments", res.data);
+        //     this.setState({
+        //         comments: res.data
+        //     })
+        // })
     }
 
     render() {
-        // const { id } = this.props.location.state
+        const { sourceUrl, title, img, servings } = this.state.recipe;
+        const { Ingredients, Instructions, topIngredients, topInstructions, comments } = this.state
+        const ingredients = Ingredients.map(ingredient => {
+            return <ul>
+                <li>
+                    {ingredient.originalString}
+                </li>
+            </ul>
+        })
+        const instructions = Instructions.map(instruction => {
+            return <ul>
+                <li>
+                    {instruction}
+                </li>
+            </ul>
+        })
+
         return (
-            <h1>Develop Page!</h1>
+            <Container fluid>
+            {/* header */}
+                <Row>
+                    <Header>
+                        {title}
+                    </Header>
+                </Row>
+            {/* Recipe */}
+                <Row>
+                    <Col size="5">
+                        {ingredients}
+                    </Col>
+                    <Col size="5">
+                        {instructions}
+                    </Col>
+                </Row>
+            
+                <Row>
+
+                    {/* Comments */}
+
+                </Row>
+            </Container>
+            
         )}
 
 }
