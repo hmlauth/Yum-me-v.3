e7240688f11e7f6e1fd508f97d598336b56089b0
lerodcalanoc <lerodcalanoc@gmail.com>
2019-05-18 17:25:53 -0700

    fixed - guess user will not be able to access landing page

--- client/src/pages/Landing/landing.js
+++ client/src/pages/Landing/landing.js
@@ -1,20 +1,78 @@
 import React, { Component } from "react";
-import Saved from "../../components/SavedRecipes/Saved"
+import Saved from "../../components/SavedRecipes/Saved";
 import Search from "../../components/FeaturedRecipes/Search";
-import Youtube from "../../components/Youtube"
-
+import Youtube from "../../components/Youtube";
+import { Button } from "reactstrap";
+import { Link } from "react-router-dom"
+import API from "../../utils/API"
+import "./landing.scss";
+import loading from '../../assets/images/loading.gif';
 
 
 class Landing extends Component {
 
+    state = {
+        loggedIn: false,
+        user: null,
+        loading: true
+    }
+
+    componentDidMount() {
+
+        this.loading();
+
+        API.isLoggedIn().then(user => {
+            if (user.data.loggedIn) {
+                this.setState({
+                    loggedIn: true,
+                    user: user.data.user
+                });
+            }
+        }).catch(err => {
+            console.log(err);
+        });
+
+        console.log(this.props)
+    }
+
+    loading() {
+        setTimeout(() => {
+            this.setState({
+                loading: false
+            })
+        }, 3000)
+    }
+
     render() {
         return (
-            <div>
-                <Saved />
-                <Search />
-                <Youtube />
+            <div className="landingPage">
+                {this.state.loggedIn ? (
+                     <div>
+                        <Saved />
+                        <Search />
+                        <Youtube />
+                    </div>
+                    
+                ) : (
+                        <div className="noUser">
+                            {!this.state.loading ? (
+                                <>
+                                    <h1>Please log in.</h1>
+                                    <Link className="loginLink" to="/login">
+                                    <Button className="loginBtn" color="info" block>Login</Button>
+                                    </Link>
+                               <br></br>
+                                </>
+                            ) : (
+
+                                <img src={loading} className="loadingIcon" alt="loading"></img>
+
+                                )}
+                        </div>
+                    )}
             </div>
-        )}
+        )
+    }
 
 }
 
