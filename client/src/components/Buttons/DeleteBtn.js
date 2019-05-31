b2fc1387c488d87b5ff9b328e4697388e660d12d
Andromeda <hannalauth@gmail.com>
2019-05-31 00:02:28 -0700

    continued removing console logs

--- client/src/components/Buttons/DeleteBtn.js
+++ client/src/components/Buttons/DeleteBtn.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 export const DeleteBtn = props => {
-  console.log(props.id);
-  console.log(props.onClick)
   return (
     <button className="btn btn-warn delete-btn" 
       onClick={() => props.onClick(props.id)} >
--- client/src/components/Buttons/SaveBtn.js
+++ client/src/components/Buttons/SaveBtn.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 export const SaveBtn = props => {
-  console.log("SaveBtn PROPS.RECIPE", props.recipe);
   return (
-
     <button className="btn btn-success save-btn" 
       onClick={ () => props.onClick(props.recipe)} >
         Save
